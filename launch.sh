12e84f7b3ac55568acbb5f5403e3bfd7d6d8595f
Jason McLeod <jason@mcleods.me>
2025-10-11 09:18:48 -0400

    Fix for server file change

--- launch.sh
+++ launch.sh
@@ -15,13 +15,7 @@ fi
 if ! [[ -f 'Astropolis_server_pack.zip' ]]; then
 	rm -fr config kubejs libraries mods *.zip forge*.jar
 	curl -Lo 'Astropolis_server_pack.zip' 'https://edge.forgecdn.net/files/5244/63/Astropolis_server_pack.zip' && unzip -u -o 'Astropolis_server_pack.zip' -d /data
-	DIR_TEST=$(find . -type d -maxdepth 1 | tail -1 | sed 's/^.\{2\}//g')
-	if [[ $(find . -type d -maxdepth 1 | wc -l) -gt 1 ]]; then
-		cd "${DIR_TEST}"
-		mv -f * /data
-		cd /data
-		rm -fr "$DIR_TEST"
-	fi
+  curl -Lo forge-${FORGE_VERSION}-installer.jar 'https://maven.minecraftforge.net/net/minecraftforge/forge/'${FORGE_VERSION}'/forge-'${FORGE_VERSION}'-installer.jar'
 	java -jar $(ls forge-*-installer.jar) --installServer && rm -f forge-*-installer.jar
 fi
 
